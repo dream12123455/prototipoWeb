25603569edaff0f6751b8687eaba3f1d5511104d
dream12123455 <163029142+dream12123455@users.noreply.github.com>
2024-04-04 22:56:17 -0500

    Add files via upload

---
+++ js/funtionsLogin.js
@@ -0,0 +1,21 @@
+var userEmail = document.getElementById("userPrincipal");
+var password = document.getElementById("password");
+
+var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+var expReg2 = /^[a-z0-9]^/
+
+function login(){
+
+    let tag = document.getElementById("#emailHelp")
+
+    if(expReg.test(userEmail.value) && password.value !=""){
+
+        if(userEmail.value == "bdjdl@gmail.com")
+        window.location.href = "/html/page2.html"
+        else
+        window.location.href = "/html/votaciones.html"
+
+    }else{
+        userEmail.style.border = "red solid 2px";
+    }
+}
